dcc420b38170c62f08f5b60fde7c7351dc7a1564
hurikake-dev <hurikake.dev@gmail.com>
2024-02-11 16:02:38 +0900

    remove: darkmode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,6 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {
       backgroundImage: {
